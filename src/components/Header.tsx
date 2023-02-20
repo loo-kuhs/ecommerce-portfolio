5dde92a19b656ef3e721aabea3999c56bd9d8324
lucasbear <rukazu89@gmail.com>
2023-02-19 22:44:12 -0600

    Add responsive to header and topbar components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,13 +10,7 @@ import {
   MenuItem,
   MenuList,
 } from '@chakra-ui/react'
-import {
-  HamburgerIcon,
-  AddIcon,
-  ExternalLinkIcon,
-  RepeatIcon,
-  EditIcon,
-} from '@chakra-ui/icons'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
 const menu = [
   { name: 'About us', href: '#' },
@@ -36,13 +30,9 @@ export const Header = () => {
         size='lg'
         justifyContent='space-between'
         alignItems='center'>
-        <Box margin='1rem 0'>
-          <Image src='/images/logo.svg' alt='' width={100} height={48} />
-        </Box>
-
-        <nav>
+        <Flex gap='0.5rem' margin='1rem 0'>
           {/* TODO: find a way to remove the code duplication for responsive app */}
-          <Box display={{ md: 'none' }}>
+          <Box as='nav' display={{ md: 'none' }}>
             <Menu>
               <MenuButton
                 as={IconButton}
@@ -51,50 +41,34 @@ export const Header = () => {
                 variant='outline'
               />
               <MenuList>
-                <MenuItem icon={<AddIcon />} command='⌘T'>
-                  New Tab
-                </MenuItem>
-                <MenuItem icon={<ExternalLinkIcon />} command='⌘N'>
-                  New Window
-                </MenuItem>
-                <MenuItem icon={<RepeatIcon />} command='⌘⇧N'>
-                  Open Closed Tab
-                </MenuItem>
-                <MenuItem icon={<EditIcon />} command='⌘O'>
-                  Open File...
-                </MenuItem>
+                {menu.map((item) => {
+                  return (
+                    <MenuItem key={item.name}>
+                      <Link href={item.href}>{item.name}</Link>
+                    </MenuItem>
+                  )
+                })}
               </MenuList>
             </Menu>
           </Box>
+          <Image src='/images/logo.svg' alt='' width={100} height={48} />
+        </Flex>
 
+        <Box as='nav'>
           <Flex
             as='ul'
             display={{ base: 'none', sm: 'flex' }}
             listStyleType='none'
             gap='2rem'>
-            <li>
-              <Link href='#'>About us</Link>
-            </li>
-            <li>
-              <Link href='#'>Woman</Link>
-            </li>
-            <li>
-              <Link href='#'>Men</Link>
-            </li>
-            <li>
-              <Link href='#'>Beauty</Link>
-            </li>
-            <li>
-              <Link href='#'>Accessories</Link>
-            </li>
-            <li>
-              <Link href='#'>Blog</Link>
-            </li>
-            <li>
-              <Link href='#'>Contact</Link>
-            </li>
+            {menu.map((item) => {
+              return (
+                <li key={item.name}>
+                  <Link href={item.href}>{item.name}</Link>
+                </li>
+              )
+            })}
           </Flex>
-        </nav>
+        </Box>
 
         <div className='commerce-menu'>
           <Flex as='ul' listStyleType='none' gap='1.5rem'>
